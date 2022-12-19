13a760cf90e2efa465e2e5e10bdc91e67fdf223b
Nikolay Ignatov <ignatov.99@yandex.ru>
2022-12-19 15:47:03 +0300

    Prepare project
    Delete main storyboard

---
+++ Navigation/Navigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Navigation
+//
+//  Created by Николай Игнатов on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
