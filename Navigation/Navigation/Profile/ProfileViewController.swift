93ed951c1fd4813cdb048f30d8ffa28fa4014415
Nikolay Ignatov <ignatov.99@yandex.ru>
2022-12-28 18:14:25 +0300

    add new button on profileViewController

--- Navigation/Navigation/Profile/ProfileViewController.swift
+++ Navigation/Navigation/Profile/ProfileViewController.swift
@@ -10,14 +10,49 @@ import UIKit
 
 class ProfileViewController: UIViewController{
     
-    private var profileHeaderView: ProfileHeaderView { return self.view as! ProfileHeaderView }
+   // private var profileHeaderView: ProfileHeaderView { return self.view as! ProfileHeaderView }
     
-    override func loadView() {
-        self.view = ProfileHeaderView(frame: UIScreen.main.bounds)
+    //    override func loadView() {
+    //        self.view = ProfileHeaderView(frame: UIScreen.main.bounds)
+    //    }
+    
+    private let contentView = ProfileHeaderView()
+    
+    private lazy var changeTitleButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Change title", for: .normal)
+        button.backgroundColor = .blue
+        button.tintColor = .white
+       // button.addTarget(self, action: #selector(changeTitleButtonTapped), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private func setupView() {
+        view.addSubview(contentView)
+        view.addSubview(changeTitleButton)
+        setupConstraints()
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
         title = "Профиль"
+        setupView()
     }
 }
+
+private extension ProfileViewController {
+     func setupConstraints() {
+         NSLayoutConstraint.activate([
+            contentView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            contentView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            contentView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            contentView.heightAnchor.constraint(equalToConstant: 220),
+            changeTitleButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            changeTitleButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            changeTitleButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+         ])
+    }
+}
+
