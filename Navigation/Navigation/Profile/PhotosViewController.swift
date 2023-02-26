6b0616d109c4274ec45fc52874c534c232659407
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-02-26 03:15:54 +0300

    refactor photosVC

--- Navigation/Navigation/Profile/PhotosViewController.swift
+++ Navigation/Navigation/Profile/PhotosViewController.swift
@@ -5,9 +5,14 @@
 //  Created by Николай Игнатов on 13.01.2023.
 //
 
+import iOSIntPackage
 import UIKit
 
 final class PhotosViewController: UIViewController {
+    
+    private let facade = ImagePublisherFacade()
+    
+    private var userImages: [UIImage] = []
     
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -27,7 +32,7 @@ final class PhotosViewController: UIViewController {
         view.addSubview(collectionView)
         setConstraints()
         collectionView.dataSource = self
-      
+        facade.subscribe(self)
     }
     
     override func viewDidLayoutSubviews() {
@@ -39,22 +44,33 @@ final class PhotosViewController: UIViewController {
         }
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.facade.addImagesWithTimer(time: 0.5, repeat: 20)
+        }
+    }
+    
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.navigationBar.isHidden = true
     }
+    
+    deinit {
+           facade.removeSubscription(for: self)
+       }
 }
 
 
 extension PhotosViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 20
+        return userImages.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotosCollectionViewCell", for: indexPath) as! PhotosCollectionViewCell
-        let imageName = "\(indexPath.item + 1)"
-        let image = UIImage(named: imageName)
+
+        let image = userImages[indexPath.item]
         cell.configure(with: image)
         return cell
     }
@@ -68,5 +84,12 @@ private extension PhotosViewController{
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+}
+
+extension PhotosViewController: ImageLibrarySubscriber {
+    func receive(images: [UIImage]) {
+        userImages = images
+        self.collectionView.reloadData()
     }
 }
